71e8a11874152a872dd3b98f29db7941d353c391
WangMinSSS <969821285@qq.com>
2017-05-08 11:33:05 +0800

    --2017.05.08

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print('hello,wangmin')
+import numpy as np
+import pylab as pl
+
+x = np.arange(0, 2*np.pi, 2*np.pi/1000)
+y = np.where(x < np.pi, np.sin(2*x), 0) + np.where(x >= np.pi, np.sin(4*x), 0)
+fy = np.fft.fft(y)/len(y)
+fy = np.abs(fy)
